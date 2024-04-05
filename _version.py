dc1b10ff88418fc8a05cff9f05cd09ccdbb1212a
Linnik V.V <vlinnik@mail.ru>
2024-04-05 09:07:31 +0300

    Тест инкремента версии

--- _version.py
+++ _version.py
@@ -1,7 +1,7 @@
 # Version managed by git-versioner
-version = "v0.0.1-gb936d85"
-version_short = "v0.0.1"
-git_hash = "b936d85"
+version = "v0.0.2-g5e64109"
+version_short = "v0.0.2"
+git_hash = "5e64109"
 on_tag = False
 dirty = False
 SUPPORT_PATCH = True
