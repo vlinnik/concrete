a2c0b96116ec75b8c16c4037def24bc98b60e2e4
Linnik V.V <vlinnik@mail.ru>
2024-04-09 13:15:49 +0300

    Версия обновлена до v0.1.0

--- _version.py
+++ _version.py
@@ -1,7 +1,7 @@
 # Version managed by git-versioner
-version = "v0.0.2-g5e64109"
-version_short = "v0.0.2"
-git_hash = "5e64109"
+version = "v0.1.0-g5b5b026"
+version_short = "v0.1.0"
+git_hash = "5b5b026"
 on_tag = False
 dirty = False
 SUPPORT_PATCH = True
