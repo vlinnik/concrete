4fe98d3494e9a85834fa7575714f35367bdc7f29
Linnik V.V <vlinnik@mail.ru>
2024-04-09 23:41:32 +0300

    Мелкие изменения

--- _version.py
+++ _version.py
@@ -1,7 +1,7 @@
 # Version managed by git-versioner
-version = "v0.1.0-g5b5b026"
-version_short = "v0.1.0"
-git_hash = "5b5b026"
+version = "v0.1.1-g0c42466"
+version_short = "v0.1.1"
+git_hash = "0c42466"
 on_tag = False
 dirty = False
 SUPPORT_PATCH = True
